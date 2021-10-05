4650ab76e30962854e6ffe8cde093db90408d210
Peter Hamilton <peter.hamilton@shopify.com>
2021-10-05 18:11:34 -0400

    Remove test. Became invalid with swc update.
    topLevelAwait behavior changed: https://github.com/swc-project/swc/issues/2225

--- js/tests/swcify.test.ts
+++ js/tests/swcify.test.ts
@@ -55,40 +55,6 @@ describe('swcify', () => {
     }
   `);
   });
-
-  it('respects options', () => {
-    const code = swc(
-      trimmed`
-      async function f() {
-      }
-      await f();
-    `,
-      {
-        jsc: {
-          parser: {
-            syntax: 'ecmascript',
-            topLevelAwait: true,
-          },
-          target: 'es2017',
-          externalHelpers: true,
-        },
-        filename: './file.js',
-      },
-    );
-
-    expect(trim(code)).toMatch(trimmed`
-    import * as swcHelpers from \"@swc/helpers\";
-    function _f() {
-    _f = swcHelpers.asyncToGenerator(function*() {
-    });
-    return _f.apply(this, arguments);
-    }
-    function f() {
-    return _f.apply(this, arguments);
-    }
-    await f();
-  `);
-  });
 });
 
 describe('Custom AsyncTransform', () => {
