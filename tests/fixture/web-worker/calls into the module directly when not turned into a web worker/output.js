e6efaed50a93ce85e8ffeb41f31a4490d041ef93
Donny <kdy1997.dev@gmail.com>
2021-10-06 20:42:16 +0900

    Update test refs

--- tests/fixture/web-worker/calls into the module directly when not turned into a web worker/output.js
+++ tests/fixture/web-worker/calls into the module directly when not turned into a web worker/output.js
@@ -1,11 +1,10 @@
-import _worker from "/Users/kdy1/projects/swcify/test-ref-gen/node_modules/@shopify/web-worker/build/cjs/webpack-parts/loader?{\"plain\":false}!./worker";
-import { createWorkerFactory } from '@shopify/web-worker';
+import _worker from "@shopify/web-worker/webpack-loader?{\"plain\":false}!./worker";
+import { createWorkerFactory } from "@shopify/web-worker";
 const worker = createWorkerFactory(_worker)();
-
-(async () => {
-  const result = await worker.greet("world");
-  const element = document.createElement('div');
-  element.setAttribute('id', "WorkerResult");
-  element.textContent = result;
-  document.body.appendChild(element);
+(async ()=>{
+    const result = await worker.greet("world");
+    const element = document.createElement("div");
+    element.setAttribute("id", "WorkerResult");
+    element.textContent = result;
+    document.body.appendChild(element);
 })();
