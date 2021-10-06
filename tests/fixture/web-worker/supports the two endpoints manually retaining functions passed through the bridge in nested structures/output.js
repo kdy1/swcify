b485a66d04306a5c27f53a24841f7ad3c961dfdc
Donny <kdy1997.dev@gmail.com>
2021-10-06 20:53:01 +0900

    Update test refs

--- tests/fixture/web-worker/supports the two endpoints manually retaining functions passed through the bridge in nested structures/output.js
+++ tests/fixture/web-worker/supports the two endpoints manually retaining functions passed through the bridge in nested structures/output.js
@@ -1,42 +1,39 @@
-import _worker from "/Users/kdy1/projects/swcify/test-ref-gen/node_modules/@shopify/web-worker/build/cjs/webpack-parts/loader?{\"plain\":false}!./worker";
-import { createWorkerFactory } from '@shopify/web-worker'; // See previous test for details of these test utilities
-
-self.WorkerTestClass = class WorkerTestClass {};
+import _worker from "@shopify/web-worker/webpack-loader?{\"plain\":false}!./worker";
+import { createWorkerFactory } from "@shopify/web-worker";
+self.WorkerTestClass = class WorkerTestClass {
+};
 self.memoryTracker = new WeakMap();
 self.worker = createWorkerFactory(_worker)();
-
-self.retain = async () => {
-  start();
-
-  const funcOne = () => {};
-
-  const funcTwo = () => {};
-
-  self.memoryTracker.set(funcOne, new self.WorkerTestClass());
-  self.memoryTracker.set(funcTwo, new self.WorkerTestClass());
-  await self.worker.retain({
-    funcOne,
-    funcs: [funcOne, funcTwo]
-  });
-  done();
+self.retain = async ()=>{
+    start();
+    const funcOne = ()=>{
+    };
+    const funcTwo = ()=>{
+    };
+    self.memoryTracker.set(funcOne, new self.WorkerTestClass());
+    self.memoryTracker.set(funcTwo, new self.WorkerTestClass());
+    await self.worker.retain({
+        funcOne,
+        funcs: [
+            funcOne,
+            funcTwo
+        ]
+    });
+    done();
 };
-
-self.release = async () => {
-  start();
-  await self.worker.release();
-  done();
+self.release = async ()=>{
+    start();
+    await self.worker.release();
+    done();
 };
-
 done();
-
 function start() {
-  for (const node of document.querySelectorAll('#' + "WorkerResult")) {
-    node.remove();
-  }
+    for (const node of document.querySelectorAll("#" + "WorkerResult")){
+        node.remove();
+    }
 }
-
 function done() {
-  const element = document.createElement('div');
-  element.setAttribute('id', "WorkerResult");
-  document.body.appendChild(element);
+    const element = document.createElement("div");
+    element.setAttribute("id", "WorkerResult");
+    document.body.appendChild(element);
 }
