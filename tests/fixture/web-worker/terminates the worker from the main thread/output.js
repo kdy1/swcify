f9912807bbcecc4f9568252854ae7d29512d1bb6
Donny <kdy1997.dev@gmail.com>
2021-10-06 20:39:12 +0900

    Update test refs

--- tests/fixture/web-worker/terminates the worker from the main thread/output.js
+++ tests/fixture/web-worker/terminates the worker from the main thread/output.js
@@ -1,18 +1,16 @@
-import _worker from "/Users/kdy1/projects/swcify/test-ref-gen/node_modules/@shopify/web-worker/build/cjs/webpack-parts/loader?{\"plain\":false}!./worker";
-import { createWorkerFactory, terminate } from '@shopify/web-worker';
+import _worker from "@shopify/web-worker/webpack-loader?{\"plain\":false}!./worker";
+import { createWorkerFactory, terminate } from "@shopify/web-worker";
 self.worker = createWorkerFactory(_worker)();
-
-(async () => {
-  const result = await self.worker.greet();
-  const element = document.createElement('div');
-  element.setAttribute('id', "WorkerResult");
-  element.textContent = result;
-  document.body.appendChild(element);
+(async ()=>{
+    const result = await self.worker.greet();
+    const element = document.createElement("div");
+    element.setAttribute("id", "WorkerResult");
+    element.textContent = result;
+    document.body.appendChild(element);
 })();
-
-self.terminateWorker = () => {
-  terminate(self.worker);
-  const element = document.createElement('div');
-  element.setAttribute('id', "Terminate");
-  document.body.appendChild(element);
+self.terminateWorker = ()=>{
+    terminate(self.worker);
+    const element = document.createElement("div");
+    element.setAttribute("id", "Terminate");
+    document.body.appendChild(element);
 };
