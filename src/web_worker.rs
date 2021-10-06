3b1637382ca14bdae7295ffc9843ff5865a02e70
Donny <kdy1997.dev@gmail.com>
2021-10-06 20:32:35 +0900

    Fix imports and extract methods

--- src/web_worker.rs
+++ src/web_worker.rs
@@ -1,3 +1,5 @@
+use std::mem::take;
+
 use rustc_hash::FxHashMap;
 use swc_atoms::js_word;
 use swc_common::DUMMY_SP;
@@ -7,7 +9,7 @@ use swc_ecmascript::{
         eval::{EvalResult, Evaluator},
         marks::Marks,
     },
-    utils::{ident::IdentLike, private_ident, ExprExt, Id},
+    utils::{ident::IdentLike, prepend_stmts, private_ident, ExprExt, Id},
     visit::{Fold, FoldWith, Node, Visit, VisitWith},
 };
 
@@ -30,6 +32,57 @@ struct Data {
     create_worker_factory: FxHashMap<Id, bool>,
 }
 
+impl WebWorker {
+    fn extract_import_from_arrow_arg(&mut self, args: &[ExprOrSpread]) -> Option<Str> {
+        if args.len() == 1 && args[0].spread.is_none() {
+            match &*args[0].expr {
+                Expr::Arrow(ArrowExpr {
+                    params,
+                    body: BlockStmtOrExpr::Expr(body),
+                    is_async: false,
+                    is_generator: false,
+                    ..
+                }) if params.is_empty() => {
+                    // We are in some arrow like `() => expr`.
+                    //
+                    // Now we check if the `expr` is a call to import.
+
+                    match &**body {
+                        Expr::Call(CallExpr {
+                            callee: ExprOrSuper::Expr(callee),
+                            args,
+                            ..
+                        }) => {
+                            if callee.is_ident_ref_to(js_word!("import")) {
+                                // We are in some arrow like `() => import(<args>)`.
+
+                                if args.len() == 1 && args[0].spread.is_none() {
+                                    let eval_res = self.eval.as_mut().unwrap().eval(&args[0].expr);
+
+                                    match eval_res {
+                                        Some(EvalResult::Lit(Lit::Str(s))) => return Some(s),
+                                        res => {
+                                            panic!("Failed to evaluate a dynamic import to a string to create a web worker: eval result = {:?}",res)
+                                        }
+                                    }
+                                } else {
+                                    // TODO(kdy1): Should we report an error?
+                                }
+                            }
+                        }
+
+                        _ => {}
+                    }
+                }
+
+                _ => {}
+            }
+        }
+
+        None
+    }
+}
+
 impl Fold for WebWorker {
     // TODO(kdy1): Apply this after https://github.com/swc-project/swc/pull/2347 is merged
     // noop_fold_type!()
@@ -38,98 +91,64 @@ impl Fold for WebWorker {
         let mut e = e.fold_children_with(self);
 
         match &e.callee {
-            ExprOrSuper::Expr(callee) => match &**callee {
-                Expr::Ident(callee) => {
-                    let callee_span = callee.span;
+            ExprOrSuper::Expr(callee) => {
+                match &**callee {
+                    Expr::Ident(callee) => {
+                        let callee_span = callee.span;
 
-                    // This is a call to createWorkerFactory
-                    if let Some(plain) = self
-                        .data
-                        .create_worker_factory
-                        .get(&callee.to_id())
-                        .copied()
-                    {
-                        if e.args.len() == 1 && e.args[0].spread.is_none() {
-                            match &mut *e.args[0].expr {
-                                Expr::Arrow(ArrowExpr {
-                                    params,
-                                    body: BlockStmtOrExpr::Expr(body),
-                                    is_async: false,
-                                    is_generator: false,
-                                    ..
-                                }) if params.is_empty() => match &mut **body {
-                                    Expr::Call(CallExpr {
-                                        callee: ExprOrSuper::Expr(callee),
-                                        args,
-                                        ..
-                                    }) => {
-                                        if callee.is_ident_ref_to(js_word!("import")) {
-                                            if args.len() == 1 && args[0].spread.is_none() {
-                                                match self
-                                                    .eval
-                                                    .as_mut()
-                                                    .unwrap()
-                                                    .eval(&args[0].expr)
-                                                {
-                                                    Some(EvalResult::Lit(Lit::Str(s))) => {
-                                                        // import workerStuff from '@shopify/web-worker/webpack-loader!./worker';
-                                                        // createWorkerFactory(workerStuff);
+                        // This is a call to createWorkerFactory
+                        if let Some(plain) = self
+                            .data
+                            .create_worker_factory
+                            .get(&callee.to_id())
+                            .copied()
+                        {
+                            tracing::info!(
+                                plain = plain,
+                                "Find a call to createWorkerFactory: {}",
+                                callee,
+                            );
 
-                                                        let src = Str {
-                                                            span:s.span,
-                                                            value: format!("@shopify/web-worker/webpack-loader!{}",s.value).into(),
-                                                            has_escape: false,
-                                                            kind: Default::default(),
-                                                        };
+                            if let Some(s) = self.extract_import_from_arrow_arg(&e.args) {
+                                // import workerStuff from '@shopify/web-worker/webpack-loader!./worker';
+                                // createWorkerFactory(workerStuff);
+                                let src = Str {
+                                    span: s.span,
+                                    value: format!(
+                                        "@shopify/web-worker/webpack-loader!{}",
+                                        s.value
+                                    )
+                                    .into(),
+                                    has_escape: false,
+                                    kind: Default::default(),
+                                };
 
-                                                        let worker_fn = private_ident!("worker");
+                                let worker_fn = private_ident!("worker");
 
-                                                        args[0].expr = Box::new(Expr::Ident(
-                                                            worker_fn.clone(),
-                                                        ));
+                                e.args[0].expr = Box::new(Expr::Ident(worker_fn.clone()));
 
-                                                        let specifier = ImportSpecifier::Default(
-                                                            ImportDefaultSpecifier {
-                                                                span: DUMMY_SP,
-                                                                local: worker_fn,
-                                                            },
-                                                        );
+                                let specifier = ImportSpecifier::Default(ImportDefaultSpecifier {
+                                    span: DUMMY_SP,
+                                    local: worker_fn,
+                                });
 
-                                                        self.added_imports.push(
-                                                            ModuleItem::ModuleDecl(
-                                                                ModuleDecl::Import(ImportDecl {
-                                                                    span: callee_span,
-                                                                    specifiers: vec![specifier],
-                                                                    src,
-                                                                    type_only: Default::default(),
-                                                                    asserts: Default::default(),
-                                                                }),
-                                                            ),
-                                                        )
-                                                    }
-                                                    res => {
-                                                        panic!("Failed to evaluate a dynamic import to a string to create a web worker: eval result = {:?}",res)
-                                                    }
-                                                }
-                                            } else {
-                                                // TODO(kdy1): Should we report an error?
-                                            }
-                                        }
-                                    }
-
-                                    _ => {}
-                                },
-
-                                _ => {}
+                                self.added_imports
+                                    .push(ModuleItem::ModuleDecl(ModuleDecl::Import(ImportDecl {
+                                        span: callee_span,
+                                        specifiers: vec![specifier],
+                                        src,
+                                        type_only: Default::default(),
+                                        asserts: Default::default(),
+                                    })))
+                            } else {
+                                // TODO(kdy1): Should we report an error?
                             }
-                        } else {
-                            // TODO(kdy1): Should we report an error?
                         }
                     }
-                }
 
-                _ => {}
-            },
+                    _ => {}
+                }
+            }
             _ => {}
         }
 
@@ -150,7 +169,11 @@ impl Fold for WebWorker {
 
         self.eval = Some(Evaluator::new(m.clone(), Marks::new()));
 
-        m.fold_children_with(self)
+        let mut m = m.fold_children_with(self);
+
+        prepend_stmts(&mut m.body, take(&mut self.added_imports).into_iter());
+
+        m
     }
 }
 
