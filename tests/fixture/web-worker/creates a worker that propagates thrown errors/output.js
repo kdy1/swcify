adcdb3906f044e60cd9a423cb0a6aea41c9ee930
Donny <kdy1997.dev@gmail.com>
2021-10-06 20:54:39 +0900

    Update test refs

--- tests/fixture/web-worker/creates a worker that propagates thrown errors/output.js
+++ tests/fixture/web-worker/creates a worker that propagates thrown errors/output.js
@@ -1,19 +1,16 @@
-import _worker from "/Users/kdy1/projects/swcify/test-ref-gen/node_modules/@shopify/web-worker/build/cjs/webpack-parts/loader?{\"plain\":false}!./worker";
-import { createWorkerFactory } from '@shopify/web-worker';
+import _worker from "@shopify/web-worker/webpack-loader?{\"plain\":false}!./worker";
+import { createWorkerFactory } from "@shopify/web-worker";
 const worker = createWorkerFactory(_worker)();
-
-(async () => {
-  let content = '';
-
-  try {
-    await worker.blowUp();
-    content = 'All clear!';
-  } catch (error) {
-    content = error.message + error.stack;
-  }
-
-  const element = document.createElement('div');
-  element.setAttribute('id', "WorkerResult");
-  element.textContent = content;
-  document.body.appendChild(element);
+(async ()=>{
+    let content = "";
+    try {
+        await worker.blowUp();
+        content = "All clear!";
+    } catch (error) {
+        content = error.message + error.stack;
+    }
+    const element = document.createElement("div");
+    element.setAttribute("id", "WorkerResult");
+    element.textContent = content;
+    document.body.appendChild(element);
 })();
