dfb450c59d06776c0886a82627834b93e31c7f1e
Donny <kdy1997.dev@gmail.com>
2021-10-06 20:43:37 +0900

    Update test refs

--- tests/fixture/web-worker/can create a worker that communicates via an iframe without same-origin credentials/output.js
+++ tests/fixture/web-worker/can create a worker that communicates via an iframe without same-origin credentials/output.js
@@ -1,14 +1,13 @@
-import _worker from "/Users/kdy1/projects/swcify/test-ref-gen/node_modules/@shopify/web-worker/build/cjs/webpack-parts/loader?{\"plain\":false}!./worker";
-import { createWorkerFactory, createIframeWorkerMessenger } from '@shopify/web-worker';
+import _worker from "@shopify/web-worker/webpack-loader?{\"plain\":false}!./worker";
+import { createWorkerFactory, createIframeWorkerMessenger } from "@shopify/web-worker";
 const worker = createWorkerFactory(_worker)({
-  createMessenger: createIframeWorkerMessenger
+    createMessenger: createIframeWorkerMessenger
 });
-
-(async () => {
-  document.cookie = "MY_COOKIE" + '=1';
-  const result = await worker.default();
-  const element = document.createElement('div');
-  element.setAttribute('id', "WorkerResult");
-  element.textContent = result;
-  document.body.appendChild(element);
+(async ()=>{
+    document.cookie = "MY_COOKIE" + "=1";
+    const result = await worker.default();
+    const element = document.createElement("div");
+    element.setAttribute("id", "WorkerResult");
+    element.textContent = result;
+    document.body.appendChild(element);
 })();
