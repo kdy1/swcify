807de7a58246269622058eebfe1f240993607a0c
Donny <kdy1997.dev@gmail.com>
2021-10-06 20:42:50 +0900

    Update test refs

--- tests/fixture/web-worker/can create a plain worker factory that can produce workers wrapping the original module/output.js
+++ tests/fixture/web-worker/can create a plain worker factory that can produce workers wrapping the original module/output.js
@@ -1,18 +1,15 @@
-import _worker from "/Users/kdy1/projects/swcify/test-ref-gen/node_modules/@shopify/web-worker/build/cjs/webpack-parts/loader?{\"plain\":true}!./worker";
-import { createPlainWorkerFactory } from '@shopify/web-worker';
+import _worker from "@shopify/web-worker/webpack-loader?{\"plain\":true}!./worker";
+import { createPlainWorkerFactory } from "@shopify/web-worker";
 const worker = createPlainWorkerFactory(_worker)();
-
-(async () => {
-  const result = await new Promise(resolve => {
-    worker.addEventListener('message', ({
-      data
-    }) => {
-      resolve(data);
+(async ()=>{
+    const result = await new Promise((resolve)=>{
+        worker.addEventListener("message", ({ data  })=>{
+            resolve(data);
+        });
+        worker.postMessage("world");
     });
-    worker.postMessage("world");
-  });
-  const element = document.createElement('div');
-  element.setAttribute('id', "WorkerResult");
-  element.textContent = result;
-  document.body.appendChild(element);
+    const element = document.createElement("div");
+    element.setAttribute("id", "WorkerResult");
+    element.textContent = result;
+    document.body.appendChild(element);
 })();
