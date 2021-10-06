48ab799204cad210a38439089acd68121d3506d4
Donny <kdy1997.dev@gmail.com>
2021-10-06 20:40:08 +0900

    Update test refs

--- tests/fixture/web-worker/automatically proxies functions passed from the parent to the worker/output.js
+++ tests/fixture/web-worker/automatically proxies functions passed from the parent to the worker/output.js
@@ -1,16 +1,18 @@
-import _worker from "/Users/kdy1/projects/swcify/test-ref-gen/node_modules/@shopify/web-worker/build/cjs/webpack-parts/loader?{\"plain\":false}!./worker";
-import { createWorkerFactory } from '@shopify/web-worker';
+import _worker from "@shopify/web-worker/webpack-loader?{\"plain\":false}!./worker";
+import { createWorkerFactory } from "@shopify/web-worker";
 const worker = createWorkerFactory(_worker)();
-const users = [{
-  getName: () => "Gord"
-}, {
-  getName: () => "Michelle"
-}];
-
-(async () => {
-  const result = await worker.greet(users);
-  const element = document.createElement('div');
-  element.setAttribute('id', "WorkerResult");
-  element.textContent = result;
-  document.body.appendChild(element);
+const users = [
+    {
+        getName: ()=>"Gord"
+    },
+    {
+        getName: ()=>"Michelle"
+    }
+];
+(async ()=>{
+    const result = await worker.greet(users);
+    const element = document.createElement("div");
+    element.setAttribute("id", "WorkerResult");
+    element.textContent = result;
+    document.body.appendChild(element);
 })();
