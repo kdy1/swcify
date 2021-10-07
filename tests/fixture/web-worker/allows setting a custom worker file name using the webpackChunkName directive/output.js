3603c967224a0941fb663956ae55b15d244cbb97
Donny <kdy1997.dev@gmail.com>
2021-10-07 19:19:39 +0900

    Update test refs

--- tests/fixture/web-worker/allows setting a custom worker file name using the webpackChunkName directive/output.js
+++ tests/fixture/web-worker/allows setting a custom worker file name using the webpackChunkName directive/output.js
@@ -1,9 +1,8 @@
-import _worker from "/Users/kdy1/projects/swcify/test-ref-gen/node_modules/@shopify/web-worker/build/cjs/webpack-parts/loader?{\"name\":\"myFancyWorker\",\"plain\":false}!./worker";
-import { createWorkerFactory } from '@shopify/web-worker';
+import _worker from "@shopify/web-worker/webpack-loader?{\"name\":\"myFancyWorker\",\"plain\":false}!./worker";
+import { createWorkerFactory } from "@shopify/web-worker";
 const worker = createWorkerFactory(_worker)();
-
-(async () => {
-  const element = document.createElement('div');
-  element.setAttribute('id', "WorkerResult");
-  document.body.appendChild(element);
+(async ()=>{
+    const element = document.createElement("div");
+    element.setAttribute("id", "WorkerResult");
+    document.body.appendChild(element);
 })();
